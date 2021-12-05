7888afb3e0985a5d3e141ac59c7ec9232eccf4c6
nirmal-ram <nirmal@mozark.ai>
2021-12-05 21:06:59 +0530

    added react-netflix-design

--- client-app/src/components/navbar.js
+++ client-app/src/components/navbar.js
@@ -6,7 +6,7 @@ function navbar() {
     return (
         <div className="navbar">
             <div className="logo">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/MX_Player_Logo.png" alt="logo" width="200px"/>
+                <img src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="logo" width="200px"/>
             </div>
             <SearchBar/>
             <div className="menu">
