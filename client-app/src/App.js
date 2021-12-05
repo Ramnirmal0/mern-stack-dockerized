319ddfcd7312f012d50cec99f4fbe621ab829714
nirmal-ram <nirmal@mozark.ai>
2021-12-05 22:00:38 +0530

    ui minor changes

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
+      <div className="welcomemsg">
+        <h1>Welcome User !</h1>
+        <p>We handpicked few movies that you might interested</p>
+        </div>
+      
       <MovieList/>
     </div>
   );
